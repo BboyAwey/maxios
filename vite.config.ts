541619226fad8908f1514f56b714830b951affd4
awey <bboyawey@sina.cn>
2022-12-19 16:46:59 +0800

    1.1.2

--- vite.config.ts
+++ vite.config.ts
@@ -8,17 +8,13 @@ export default defineConfig({
     outDir: 'lib',
     minify: false,
     lib: {
-      entry: resolve(__dirname, './maxios/index.ts'),
+      entry: resolve(__dirname, './src/index.ts'),
       name: packageInfo.name,
       formats: ['es', 'umd', 'cjs'],
       fileName: 'maxios'
     }
   },
-  plugins: [
-    dts({
-      entryRoot: resolve(__dirname, './maxios')
-    })
-  ],
+  plugins: [dts()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
