66fe08e4e98b019045281b63e59fa735d13aace9
awey <bboyawey@sina.cn>
2025-05-26 09:54:50 +0800

    fix: body data will be unexpectly convert to object when giving an array at any level

--- demo/index.ts
+++ demo/index.ts
@@ -26,12 +26,13 @@ globalConfig<Response<unknown>>(
   },
   {
     expect (response) {
-      return response.data.code === 1
+      return response.data.code === 0
     },
     extractor (response) {
       return response.data.data
     },
-    loading: status => console.log('global loading...', status)
+    loading: status => console.log('global loading...', status),
+    error: (data) => console.log('global error...', data),
   }
 )
 
@@ -50,7 +51,7 @@ const apis = {
     return request<void, {
       code: number
       msg: string
-      data: { shit: number }
+      data: [{ shit: number }]
     }>({
       url: '/get-shit'
     }, {
@@ -73,15 +74,28 @@ const apis = {
   },
   getShit2 () {
     return request({
-      url: '/get-shit-2'
+      url: '/get-shit-2',
+    })
+  },
+  postShit () {
+    return request({
+      url: '/post-shit',
+      method: 'POST',
+      data: [{a: 1}, {a: 2}]
+    })
+  },
+  getBizErr () {
+    return request({
+      url: '/biz-error-shit'
     })
   }
 }
 
 function sendRequest () {
-  apis.getShit2()
-  apis.getShit()
-  apis.getShit2()
+  // apis.getShit2()
+  // apis.getShit()
+  // apis.getShit2()
+  apis.postShit()
 }
 
 function sendRequest2 () {
@@ -91,19 +105,37 @@ function sendRequest2 () {
     .loading(res => console.log('loading 3...', res))
 }
 
+function sendRequest3 () {
+  apis.getShit()
+    .success(() => {
+      console.log('get shit success')
+      throw new Error('shit')
+    })
+}
+
+function sendRequest4 () {
+  apis.getBizErr()
+}
+
 window.addEventListener('load', () => {
   const button = document.createElement('button')
   button.innerText = '发送请求'
   button.addEventListener('click', sendRequest)
-
   document.body.appendChild(button)
 
   const button2 = document.createElement('button')
   button2.innerText = '发送请求2'
   button2.addEventListener('click', sendRequest2)
-
   document.body.appendChild(button2)
-  
-})
 
+  const button3 = document.createElement('button')
+  button3.innerText = '发送请求3'
+  button3.addEventListener('click', sendRequest3)
+  document.body.appendChild(button3)
+
+  const button4 = document.createElement('button')
+  button4.innerText = '发送请求4'
+  button4.addEventListener('click', sendRequest4)
+  document.body.appendChild(button4) 
+})
 
