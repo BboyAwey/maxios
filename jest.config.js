2cd31d24d326f8c22e7bf923e9255515d8ea77e4
awey <bboyawey@sina.cn>
2026-04-13 12:42:49 +0800

    chore: migrate from jest to vitest for testing

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-}
